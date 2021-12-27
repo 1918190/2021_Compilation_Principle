b0c61080036632633430b0500d66052c3c93b0c9
Chenhao Zhai <473049712@qq.com>
2021-12-27 14:46:44 +0800

    fix：change style

--- CompilationPrinciple3_1.py
+++ CompilationPrinciple3_1.py
@@ -2,93 +2,109 @@ import sys
 import numpy as np
 import copy
 
-non_ter = []  # 非终结符
-terSymbol = []  # 终结符
-firstStack = []  # firstVT的栈
-lastStack = []  # lastVT的栈
-firstF = []  # firstVT的布尔矩阵
-lastF = []  # lastVT的布尔矩阵
 
-
-def init_first_matrix(grammar, F):
+def init_first_matrix(grammar, first_boolean_matrix):
     for gra_line in grammar:
         for i in range(len(gra_line) - 1):
             if gra_line[i] == '→' or gra_line[i] == '|':
-                if gra_line[i + 1] in terSymbol:
-                    terInd = terSymbol.index(gra_line[i + 1])
-                    nonTerInd = non_ter.index(gra_line[0])
-                    F[nonTerInd][terInd] = 'T'
-                    firstStack.append([gra_line[0], gra_line[i + 1]])
-                if gra_line[i + 1] in non_ter and i < len(gra_line) - 2:
-                    if gra_line[i + 2] in terSymbol:
-                        terInd = terSymbol.index(gra_line[i + 2])
-                        nonTerInd = non_ter.index(gra_line[0])
-                        F[nonTerInd][terInd] = 'T'
-                        firstStack.append([gra_line[0], gra_line[i + 2]])
+                if gra_line[i + 1] in terminals:
+                    ter_index = terminals.index(gra_line[i + 1])
+                    non_ter_index = non_terminals.index(gra_line[0])
+                    first_boolean_matrix[non_ter_index][ter_index] = 'T'
+                    first_stack.append([gra_line[0], gra_line[i + 1]])
+                if gra_line[i + 1] in non_terminals and i < len(gra_line) - 2:
+                    if gra_line[i + 2] in terminals:
+                        ter_index = terminals.index(gra_line[i + 2])
+                        non_ter_index = non_terminals.index(gra_line[0])
+                        first_boolean_matrix[non_ter_index][ter_index] = 'T'
+                        first_stack.append([gra_line[0], gra_line[i + 2]])
 
 
-def first(grammar, F):
-    while len(firstStack) > 0:
-        tmp = firstStack.pop()
+def calculate_first(grammar, first_boolean_matrix):
+    while len(first_stack) > 0:
+        tmp = first_stack.pop()
         for gra in grammar:
             for i in range(len(gra) - 1):
                 if (gra[i] == '→' or gra[i] == '|') and gra[i + 1] == tmp[0]:
-                    terInd = terSymbol.index(tmp[1])
-                    nonTerInd = non_ter.index(gra[0])
-                    if F[nonTerInd][terInd] == 'F':
-                        F[nonTerInd][terInd] = 'T'
-                        firstStack.append([gra[0], tmp[1]])
+                    ter_index = terminals.index(tmp[1])
+                    non_ter_index = non_terminals.index(gra[0])
+                    if first_boolean_matrix[non_ter_index][ter_index] == 'F':
+                        first_boolean_matrix[non_ter_index][ter_index] = 'T'
+                        first_stack.append([gra[0], tmp[1]])
 
 
-def initLastMatrix(grammar, F):
+def init_last_matrix(grammar, last_boolean_matrix):
     for gra_line in grammar:
         for i in range(len(gra_line)):
             if i == (len(gra_line) - 1) or (i < (len(gra_line) - 1) and gra_line[i + 1] == '|'):
-                if gra_line[i] in terSymbol:
-                    terInd = terSymbol.index(gra_line[i])
-                    nonTerInd = non_ter.index(gra_line[0])
-                    F[nonTerInd][terInd] = 'T'
-                    lastStack.append([gra_line[0], gra_line[i]])
-                if gra_line[i] in non_ter and i > 0:
-                    if gra_line[i - 1] in terSymbol:
-                        terInd = terSymbol.index(gra_line[i - 1])
-                        nonTerInd = non_ter.index(gra_line[0])
-                        F[nonTerInd][terInd] = 'T'
-                        lastStack.append([gra_line[0], gra_line[i - 1]])
+                if gra_line[i] in terminals:
+                    ter_index = terminals.index(gra_line[i])
+                    non_ter_index = non_terminals.index(gra_line[0])
+                    last_boolean_matrix[non_ter_index][ter_index] = 'T'
+                    last_stack.append([gra_line[0], gra_line[i]])
+                if gra_line[i] in non_terminals and i > 0:
+                    if gra_line[i - 1] in terminals:
+                        ter_index = terminals.index(gra_line[i - 1])
+                        non_ter_index = non_terminals.index(gra_line[0])
+                        last_boolean_matrix[non_ter_index][ter_index] = 'T'
+                        last_stack.append([gra_line[0], gra_line[i - 1]])
 
 
-def last(grammar, F):
-    while len(lastStack) > 0:
-        tmp = lastStack.pop()
+def calculate_last(grammar, last_boolean_matrix):
+    while len(last_stack) > 0:
+        tmp = last_stack.pop()
         for gra in grammar:
             for i in range(len(gra)):
                 if (i == (len(gra) - 1) or (i < (len(gra) - 1) and gra[i + 1] == '|')) and gra[i] == tmp[0]:
-                    terInd = terSymbol.index(tmp[1])
-                    nonTerInd = non_ter.index(gra[0])
-                    if F[nonTerInd][terInd] == 'F':
-                        F[nonTerInd][terInd] = 'T'
-                        lastStack.append([gra[0], tmp[1]])
+                    ter_index = terminals.index(tmp[1])
+                    non_ter_index = non_terminals.index(gra[0])
+                    if last_boolean_matrix[non_ter_index][ter_index] == 'F':
+                        last_boolean_matrix[non_ter_index][ter_index] = 'T'
+                        last_stack.append([gra[0], tmp[1]])
 
 
-def printVT():
-    for firstVT in non_ter:
+def print_vt(first_boolean_matrix, last_boolean_matrix):
+    for firstVT in non_terminals:
         print("FIRSTVT(" + firstVT + ")={", end="")
-        nonTerInd = non_ter.index(firstVT)
-        for i in range(len(firstF[nonTerInd])):
-            if (firstF[nonTerInd][i] == 'T'):
-                if (i != 0):
+        non_ter_index = non_terminals.index(firstVT)
+        for i in range(len(first_boolean_matrix[non_ter_index])):
+            if first_boolean_matrix[non_ter_index][i] == 'T':
+                if i != 0:
                     print(',', end="")
-                print(terSymbol[i], end="")
+                print(terminals[i], end="")
         print("}")
-    for lastVT in non_ter:
+    for lastVT in non_terminals:
         print("LASTVT(" + lastVT + ")={", end="")
-        nonTerInd = non_ter.index(lastVT)
-        for i in range(len(lastF[nonTerInd])):
-            if (lastF[nonTerInd][i] == 'T'):
-                if (i != 0):
+        non_ter_index = non_terminals.index(lastVT)
+        for i in range(len(last_boolean_matrix[non_ter_index])):
+            if last_boolean_matrix[non_ter_index][i] == 'T':
+                if i != 0:
                     print(',', end="")
-                print(terSymbol[i], end="")
+                print(terminals[i], end="")
         print("}")
+
+
+def calculate_first_and_last(grammar):
+    boolean_matrix = np.full((len(non_terminals), len(terminals)), 'F')
+    first_boolean_matrix = copy.deepcopy(boolean_matrix)
+    last_boolean_matrix = copy.deepcopy(boolean_matrix)
+    print("first_boolean_matrix:")
+    print(first_boolean_matrix)
+    init_first_matrix(grammar, first_boolean_matrix)
+    print("first_boolean_matrix:")
+    print(first_boolean_matrix)
+    calculate_first(grammar, first_boolean_matrix)
+    print("first_boolean_matrix:")
+    print(first_boolean_matrix)
+    print("last_boolean_matrix:")
+    print(last_boolean_matrix)
+    init_last_matrix(grammar, last_boolean_matrix)
+    print("last_boolean_matrix:")
+    print(last_boolean_matrix)
+    calculate_last(grammar, last_boolean_matrix)
+    print("last_boolean_matrix:")
+    print(last_boolean_matrix)
+    print_vt(first_boolean_matrix, last_boolean_matrix)
 
 
 # 输入示例：
@@ -96,16 +112,22 @@ def printVT():
 # T→T,S|S
 # Ctrl+D结束输入
 if __name__ == '__main__':
+    non_terminals = []  # 非终结符
+    terminals = []  # 终结符
+    first_stack = []  # firstVT的栈
+    last_stack = []  # lastVT的栈
+    first_boolean_matrix = []  # firstVT的布尔矩阵
+    last_boolean_matrix = []  # lastVT的布尔矩阵
     print("请输入一个文法：")
     grammar = sys.stdin.read().splitlines()
     grammar = [e for e in grammar if e != '']
     print(grammar)
     for gr in grammar:
-        if gr[0] not in non_ter:
-            non_ter.append(gr[0])
+        if gr[0] not in non_terminals:
+            non_terminals.append(gr[0])
     for gr in grammar:
         for i in range(len(gr)):
-            if gr[i] not in non_ter and gr[i] not in terSymbol:
+            if gr[i] not in non_terminals and gr[i] not in terminals:
                 # 箭头输入是->
                 # if gr[i] == '-':
                 #     if i == len(gr)-1:
@@ -117,26 +139,7 @@ if __name__ == '__main__':
                 #         terSymbol.append(gr[i])
                 # 箭头输入是→
                 if gr[i] != '|' and gr[i] != '→' and gr[i] != ' ':
-                    terSymbol.append(gr[i])
-    print(non_ter)
-    print(terSymbol)
-    F = np.full((len(non_ter), len(terSymbol)), 'F')
-    firstF = copy.deepcopy(F)
-    lastF = copy.deepcopy(F)
-    print("firstF:")
-    print(firstF)
-    init_first_matrix(grammar, firstF)
-    print("firstF:")
-    print(firstF)
-    first(grammar, firstF)
-    print("firstF:")
-    print(firstF)
-    print("lastF:")
-    print(lastF)
-    initLastMatrix(grammar, lastF)
-    print("lastF:")
-    print(lastF)
-    last(grammar, lastF)
-    print("lastF:")
-    print(lastF)
-    printVT()
+                    terminals.append(gr[i])
+    print(non_terminals)
+    print(terminals)
+    calculate_first_and_last(grammar)
